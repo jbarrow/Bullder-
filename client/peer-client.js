525e2bfc11c44ed9dbf8a9124b7a5544f85c4fd3
Joe Barrow <joseph.d.barrow@gmail.com>
2015-02-28 16:23:05 -0500

    Fixed merge

--- client/peer-client.js
+++ client/peer-client.js
@@ -1,11 +1,7 @@
-
-
 function establishConnection() {
-	
-	var peer = new Peer({key: 'lwjd5qra8257b9'});
+	var peer = new Peer('19234', {host: 'localhost', port: 9000, path: ''});
 
 	peer.on('open', function(id) {
-
 		console.log('My peer ID is: ' + id);
 	});
 
