59e4031ca440867d999bf3932df418035afade17
Michael Snider <msilvprog7@gmail.com>
2015-02-28 18:50:34 -0500

    Pass id and geo position to master peer

--- client/peer-client.js
+++ client/peer-client.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var peer = new Peer({key: 'lwjd5qra8257b9', debug: 3});
+var client_id = 0;
 
 peer.on('open', function(id) {
 	console.log(id);
+	client_id = id;
 });
 
 peer.on('error', function(err) {
@@ -12,5 +14,6 @@ peer.on('error', function(err) {
 
 var conn = peer.connect('master-3', {reliable: true});
 conn.on('open', function() {
-	conn.send('Test!');
+	conn.send({id: client_id, pos: [window.position.coords.latitude, window.position.coords.longitude]});
 });
+
