c8f902c2c7ba09009145c718d807c341aeeb5ff1
Hunter Leath <h@hunterleath.com>
2015-02-28 17:05:27 -0500

    Updated Design of Website

--- client/app.js
+++ client/app.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+   var found_location = function(pos) {
+       console.dir(pos);
+   }
+   navigator.geolocation.getCurrentPosition(found_location); 
+});
+
 var bullderApp = angular.module('bullderApp', ["ngRoute"]).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/new', {
         templateUrl: "partials/new.html",
@@ -5,30 +12,64 @@ var bullderApp = angular.module('bullderApp', ["ngRoute"]).config(['$routeProvid
     }).when('/new/upload', {
         templateUrl: "partials/newUpload.html",
         controller: "BullderNewController",
+    }).when('/view/:id', {
+        templateUrl: "partials/info.html",
+        controller: "BullderViewController",
     }).otherwise({
        templateUrl: "partials/home.html",
         controller: "BullderController",
     });
 }]);
 
+var mockComments = [
+    {
+        id: 0,
+        text: "Hello, world",
+        op: false,
+        time: "1 minute ago",
+    },
+    {
+        id: 1,
+        text: "This is a cool comment.",
+        op: true,
+        time: "Just now",
+    }
+];
+
+var mockData = [
+    {
+        id: 0,
+        time: "4 hours ago",
+        distance: "< 1 mile away",
+        comments: mockComments,
+        title: "Pellentesque dapibus suscipit ligula.  Donec posuere augue in quam.",
+        photo: "http://placehold.it/420x320",
+    },
+    {
+        id: 1,
+        time: "4 hours ago",
+        distance: "< 1 mile away",
+        comments: mockComments,
+        title: "Pellentesque dapibus suscipit ligula.  Donec posuere augue in quam.",
+        data: {
+            extension: "pdf",
+            size: "4MB",
+            download: "http://google.com",
+        }
+    },
+]
+
 bullderApp.controller('BullderNewController', ['$scope', function($scope) {
     
 }]);
 
+bullderApp.controller('BullderViewController', ['$scope', '$route', function($scope, $route) {
+    $scope.data = mockData[$route.current.params["id"]];
+}]);
+
 bullderApp.controller('BullderController', ['$scope', function($scope) {
     $scope.view = "new";
-    $scope.data = [{
-        time: "4 hours ago",
-        distance: "< 1 mile away",
-        comments: ["hi"],
-        title: "Pellentesque dapibus suscipit ligula.  Donec posuere augue in quam.",
-        photo: "http://placehold.it/420x320",
-        // data: {
-        //     extension: "pdf",
-        //     size: "4MB",
-        //     download: "http://google.com",
-        // }
-    }];
+    $scope.data = mockData;
 }]);
 
 bullderApp.directive('bullderViewer', function() {
